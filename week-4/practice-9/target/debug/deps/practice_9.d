8b08b64c85c753d832b7dfa00f8403e4d8616ca9
kailota Igwe <igwekailota@gmail.com>
2025-10-22 22:20:01 +0100

    (Counting numbers)

---
+++ week-4/practice-9/target/debug/deps/practice_9.d
@@ -0,0 +1,5 @@
+C:\Users\NNAMDI\OneDrive\Documents\K.IgweCSC101\week-4\practice-9\target\debug\deps\practice_9.d: src\main.rs
+
+C:\Users\NNAMDI\OneDrive\Documents\K.IgweCSC101\week-4\practice-9\target\debug\deps\practice_9.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice-9/target/debug/practice-9.d
@@ -0,0 +1,1 @@
+C:\Users\NNAMDI\OneDrive\Documents\K.IgweCSC101\week-4\practice-9\target\debug\practice-9.exe: C:\Users\NNAMDI\OneDrive\Documents\K.IgweCSC101\week-4\practice-9\src\main.rs
