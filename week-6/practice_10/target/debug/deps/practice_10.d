9838506c7a9464edcefcfa7d4c83b578c813dfdf
kailota Igwe <igwekailota@gmail.com>
2025-11-05 16:49:59 +0100

    to display a true or false

---
+++ week-6/practice_10/target/debug/deps/practice_10.d
@@ -0,0 +1,5 @@
+C:\Users\NNAMDI\OneDrive\Documents\K.IgweCSC101\week-6\practice_10\target\debug\deps\practice_10.d: src\main.rs
+
+C:\Users\NNAMDI\OneDrive\Documents\K.IgweCSC101\week-6\practice_10\target\debug\deps\practice_10.exe: src\main.rs
+
+src\main.rs:
